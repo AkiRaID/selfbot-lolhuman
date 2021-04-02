f7d9c1a4d64ac8374c4999941e7303d53bb7bae5
AkiRa <79385860+AkiRaID@users.noreply.github.com>
2021-04-02 22:17:29 +0700

    Add files via upload

---
+++ language/ind.js
@@ -0,0 +1,17 @@
+const a = '```'
+
+exports.wrongf = () => {
+	return`*format salah/text kosong*`
+}
+
+exports.noregis = () => {
+	return`*「 BELUM DAFTAR 」*\n\n*cara daftar ${prefix}daftar nama|umur* \n*contoh ${prefix}daftar Akira|17*`
+}
+
+exports.rediregis = () => {
+	return`*「 SUDAH DAFTAR 」*\n\n*kamu sudah terdaftar di database bot*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`*「 DATA NEGARA 」*\n\nkamu sudah terdaftar dengan data \n\n┏━⊱nama\n┗⊱${namaUser}\n┏━⊱nomer\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱umur\n┗⊱${umurUser}\n┏━⊱waktu pendaftaran\n┗⊱${time}\n\n┏━❉ *NS* ❉━\n┣⊱${serialUser}\n┗⊱NOTE : Jangan lupa sv Nomer Owner Bot Yoo:v`
+}
